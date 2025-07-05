7ac2da12c0172556852f4a906a7e7c25e1713102
Sparsh Verma <sparshv70@gmail.com>
2025-07-05 10:22:34 +0530

    Corrected some code loss in graphql queries and mutations

--- api/graphql/mutations/user/index.ts
+++ api/graphql/mutations/user/index.ts
@@ -10,3 +10,61 @@ export const UPDATE_USER_BY_EMAIL = `
     }
   }
 `;
+
+export const FOLLOW_USER = `
+  mutation FollowUser($source:String!,$target:String!){
+    followUser(source: $source, target: $target) {
+      id
+      name
+      profilePicture
+      bio
+      username
+      email
+      following {
+        id
+        name
+        profilePicture
+        bio
+        username
+        email
+      }
+      followers {
+        id
+        name
+        profilePicture
+        bio
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const UNFOLLOW_USER = `
+  mutation UnFollowUser($source:String!,$target:String!){
+    unfollowUser(source: $source, target: $target) {
+      id
+      name
+      profilePicture
+      bio
+      username
+      email
+      following {
+        id
+        name
+        profilePicture
+        bio
+        username
+        email
+      }
+      followers {
+        id
+        name
+        profilePicture
+        bio
+        username
+        email
+      }
+    }
+  }
+`;
